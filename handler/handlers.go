a13da3f995bc376822fbed9c6698c2cb17cf7587
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-08 13:59:31 +0100

    Update handlers.go
    - Added Name to urlCreationRequest
    - Added type userSignupRequest
    - Added type userLoginRequest
    - Return status codes from store.SaveURL()
    - Added CreateUser()
    - Added CheckUserLogin()
    - Added NotFound()

--- handler/handlers.go
+++ handler/handlers.go
@@ -8,31 +8,129 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type UrlCreationRequest struct {
-	LongUrl string `json:"long_url" binding:"required"`
-	UserId  string `json:"user_id" binding:"required"`
+type urlCreationRequest struct {
+	Name    string `json:"name" binding:"required"`
+	LongURL string `json:"long_url" binding:"required"`
+	UserID  string `json:"user_id" binding:"required"`
 }
 
-func CreateShortUrl(c *gin.Context) {
-	var creationRequest UrlCreationRequest
+type userSignupRequest struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Username  string `json:"username" binding:"required"`
+	Email     string `json:"email" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+}
+
+type userLoginRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password" binding:"required"`
+}
+
+// TODO: check every request for empty values
+// TODO: add codes to JSON data eg "status_code": "URL_CREATED"
+
+func CreateShortURL(c *gin.Context) {
+	var creationRequest urlCreationRequest
 	if err := c.ShouldBindJSON(&creationRequest); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	shortUrl := shortener.GenerateShortUrl(creationRequest.LongUrl, creationRequest.UserId)
-	store.SaveUrl(shortUrl, creationRequest.LongUrl, creationRequest.UserId)
+	shortURL := shortener.GenerateShortURL(creationRequest.LongURL, creationRequest.UserID)
+	err := store.SaveURL(shortURL, creationRequest.Name, creationRequest.LongURL, creationRequest.UserID)
+	if err != "" {
+		status := http.StatusBadRequest
+		if err == "NON_EXISTING_USER" {
+			status = http.StatusUnauthorized
+		}
+
+		c.JSON(status, gin.H{
+			"status_code": err,
+		})
+		return
+	}
 
 	host := "http://localhost:9001/"
 	c.JSON(200, gin.H{
-		"message":   "short url created successfully",
-		"short_url": host + shortUrl,
+		"message":   "Short URL created successfully",
+		"short_url": host + shortURL,
 	})
 }
 
-func RedirectShortUrl(c *gin.Context) {
-	shortUrl := c.Param("shortUrl")
-	longUrl := store.GetLongUrl(shortUrl)
+func RedirectShortURL(c *gin.Context) {
+	shortURL := c.Request.URL.Path[1:]
+	longURL := store.GetLongURL(shortURL)
 
-	c.Redirect(302, longUrl)
+	c.Redirect(301, longURL)
+}
+
+func GetUserShortenedURLs(c *gin.Context) {
+	userID := c.Request.URL.Path[12:]
+
+	urls := store.GetUserShortenedURLs(userID)
+
+	c.JSON(200, urls)
+}
+
+func CreateUser(c *gin.Context) {
+	var userData userSignupRequest
+	if err := c.ShouldBindJSON(&userData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := store.User{
+		FirstName: userData.FirstName,
+		LastName:  userData.LastName,
+		Username:  userData.Username,
+		Email:     userData.Email,
+		Password:  userData.Password,
+	}
+
+	token := store.SaveUser(user)
+
+	if token != "" {
+		c.JSON(200, gin.H{
+			"message": "User created successfully",
+			"token":   token,
+		})
+	} else {
+		c.JSON(401, gin.H{
+			"message": "Something went wrong",
+		})
+	}
+}
+
+func CheckUserLogin(c *gin.Context) {
+	var userData userLoginRequest
+	if err := c.ShouldBindJSON(&userData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := store.User{
+		Username: userData.Username,
+		Email:    userData.Email,
+		Password: userData.Password,
+	}
+
+	token := store.CheckLogin(user)
+	if token != "" {
+		c.JSON(200, gin.H{
+			"message": "User logged in successfully",
+			"token":   token,
+		})
+	} else {
+		c.JSON(401, gin.H{
+			"message": "Something went wrong",
+		})
+	}
+}
+
+func NotFound(c *gin.Context) {
+	c.JSON(404, gin.H{
+		"message": "Not found",
+	})
 }
