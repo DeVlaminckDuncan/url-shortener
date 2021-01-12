c28d64092fd2ef21a713bff8c5c87f29a9849865
Duncan <duncan.de.vlaminck@student.howest.be>
2021-01-12 16:50:00 +0100

    Changed JSON name to shortenedURL

--- store/ShortenedURLData.go
+++ store/ShortenedURLData.go
@@ -2,6 +2,6 @@ package store
 
 // ShortenedURLData contains a list of ShortenedURLs and analytics (a list of ShortenedURLVisitsHistory)
 type ShortenedURLData struct {
-	ShortenedURLObject ShortenedURL `json:"shortenedURLObject"`
+	ShortenedURLObject ShortenedURL `json:"shortenedURL"`
 	Analytics          []string     `json:"analytics"`
 }
